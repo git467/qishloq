5d75478d378bd620b328f5f0ffadb2603b29c5e9
git467 <111969142+git467@users.noreply.github.com>
2026-05-09 15:37:14 +0100

    Implement image click event to manage active state
    Add image click event handling to toggle active state.

--- bir.js
+++ bir.js
@@ -14,3 +14,32 @@ images.forEach(img => {
 closeBtn.onclick = function () {
   modal.style.display = "none";
 };
+
+// Manipulate images with JavaScript
+
+const bookImages = document.querySelectorAll(".book-img");
+
+bookImages.forEach(img => {
+  img.addEventListener("click", (e) => {
+
+    // bodyga 
+    e.stopPropagation();
+
+    // boshqalardan activeni olib tashlash
+    bookImages.forEach(other => {
+      if (other !== img) {
+        other.classList.remove("active");
+      }
+    });
+
+    // activeni toggle qilish
+    img.classList.toggle("active");
+    
+  });
+});
+
+body.addEventListener("click", () => {
+  bookImages.forEach(img => {
+    img.classList.remove("active");
+  })
+})
